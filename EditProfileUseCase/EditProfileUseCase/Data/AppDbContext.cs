9127a7cffd462342c61451b8b554a5c981cafa26
VasileiosChalvatzidopoulos <t8220164@aueb.gr>
2025-02-18 19:16:09 +0200

    test

---
+++ EditProfileUseCase/EditProfileUseCase/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using EditProfileUseCase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EditProfileUseCase.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Users> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Users>(entity =>
+            {
+                entity.ToTable("travelers");
+
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).HasColumnName("userid").ValueGeneratedOnAdd();
+                entity.Property(e => e.UserName).HasColumnName("UserName");
+                entity.Property(e => e.Password).HasColumnName("Password");
+                entity.Property(e => e.Email).HasColumnName("Email");
+                entity.Property(e => e.FirstName).HasColumnName("FirstName");
+                entity.Property(e => e.LastName).HasColumnName("LastName");
+                entity.Property(e => e.Country).HasColumnName("OriginCountry");
+            });
+        }
+    }
+}
---
+++ EditProfileUseCase/EditProfileUseCase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EditProfileUseCase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
