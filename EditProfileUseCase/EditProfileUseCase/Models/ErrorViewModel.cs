b2e2a91cfbcf5570df8531ee7b2e413ddd8f5224
VasileiosChalvatzidopoulos <t8220164@aueb.gr>
2025-02-18 19:24:05 +0200

    test

--- EditProfileUseCase/EditProfileUseCase/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EditProfileUseCase.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- OptionalUseCase/OptionalUseCase/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OptionalUseCase.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
