024c530c86b7d378c2129f2ac8a7c8cc2401cc29
VasileiosChalvatzidopoulos <t8220164@aueb.gr>
2025-02-20 13:33:51 +0200

    CREATE LOGIN PAGE AND THE OTHER APPROPRIATES (CSS, LAYOUTS, ACCOUNTCONTROLLER)

---
+++ OptionalUseCase/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using OptionalUseCase.Data;
+using OptionalUseCase.Models;
+using OptionalUseCase.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EditProfileApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
