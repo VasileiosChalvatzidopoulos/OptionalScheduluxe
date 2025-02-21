4c245b6495adb1f976d7528e9660a8d3c2ffc4ba
VasileiosChalvatzidopoulos <t8220164@aueb.gr>
2025-02-21 19:51:49 +0200

    CREATE EDITPROFILE ACTION METHOD

--- OptionalUseCase/Controllers/AccountController.cs
+++ OptionalUseCase/Controllers/AccountController.cs
@@ -70,6 +70,83 @@ namespace EditProfileApp.Controllers
             }
             return View(model);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditProfile()
+        {
+
+            var userId = HttpContext.Session.GetString("UserId");
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Login");
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == int.Parse(userId));
+
+            if (user == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var model = new EditProfileViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.UserName,
+                Email = user.Email,
+                Country = user.Country
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(EditProfileViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var userId = HttpContext.Session.GetString("UserId");
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Login");
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == int.Parse(userId));
+
+            if (user == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.Email = model.Email;
+            user.Country = model.Country;
+
+            if (!string.IsNullOrWhiteSpace(model.Password))
+            {
+                user.Password = model.Password;
+            }
+
+            try
+            {
+                _context.Users.Update(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error updating profile: " + ex.Message);
+                return View(model);
+            }
+        }
         public IActionResult Logout()
         {
             HttpContext.Session.Clear();
